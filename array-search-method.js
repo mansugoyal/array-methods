afa3e441a0983d05650babed4c11a90fe65216da
mansugoyal <mansugoyal1234@gmail.com>
2024-06-03 20:19:51 +0530

    array search methods

--- array-search-method.js
+++ array-search-method.js
@@ -3,4 +3,16 @@
 let beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
 console.log("indexof",beasts.indexOf('camel'));
 
-console.log("lastindexof",beasts.lastIndexOf('val'))
+//array.lastIndexOf('value')
+console.log("lastindexof",beasts.lastIndexOf('camel'))
+
+
+//array.find()
+//it matches the first element of the array that satisfies the condition
+const array1 = [5, 12, 8, 130, 44];
+const found = array1.find((element) => element > 20);
+console.log("find",found);
+
+//array.findIndex()
+const found2 = array1.findIndex((element) => element > 20);
+console.log("findIndex",found2)
