4f8b2ec4b45ad6ce99951cb06d10dc9773cbc366
mansugoyal <mansugoyal1234@gmail.com>
2024-06-17 17:06:46 +0530

    star

---
+++ star.js
@@ -0,0 +1,19 @@
+//simple star pattern
+for(let i = 1; i<=5; i++){
+    let num = '';
+    for(let j = 1; j<=i; j++){
+        num += ' *';
+    }
+    console.log(num);
+}
+
+console.log("------------------------NEXT-LINE----------------------------");
+
+//simple reverse star pattern
+for(let i = 5; i>=1; i--){
+    let num = '';
+    for(let j = i; j>=1; j--){
+        num += ' *';
+    }
+    console.log(num);
+}
